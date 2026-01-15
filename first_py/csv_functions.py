865cd1647cb37f32fc10b3801c4c6d3b975aa799
Lais Kemilly <lais.kemilly@flow42.com.br>
2026-01-15 10:50:20 -0300

    correção identação

--- first_py/csv_functions.py
+++ first_py/csv_functions.py
@@ -2,7 +2,7 @@ import csv
 import os
 
 def ler_csv(arquivo: str) -> list:
-     """
+    """
     Função para ler um arquivo CSV e retornar o conteúdo como uma lista de dicionários.
          
     Args:
@@ -15,9 +15,10 @@ def ler_csv(arquivo: str) -> list:
 
     if not os.path.exists(arquivo):
         raise FileNotFoundError(f"O arquivo {arquivo} não foi encontrado.")
+
     with open(arquivo, mode='r', newline='', encoding='utf-8') as file:
         leitor = csv.DictReader(file)
         for linha in leitor:
             dados.append(linha)
-    
+
     return dados
